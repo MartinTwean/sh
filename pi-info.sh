fd00e6a2c72d9098ab95690ef9743cdf0abb4748
MartinTwean <94994890+MartinTwean@users.noreply.github.com>
2023-03-18 18:59:53 +0100

    1

---
+++ pi-info.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "* CPU temperature:"
+vcgencmd measure_temp
+
+echo -e "\n* CPU info:"
+cat /proc/cpuinfo
+
+echo -e "\n* Disk usage:"
+df -h
+
+echo -e "\n* Partitions info:"
+cat /proc/partitions
+
+echo -e "\n* Operating system details:"
+cat /etc/os-release
+
+echo -e "\n* Linux kernel version:"
+cat /proc/version
+
+echo -e "\n* Memory info:"
+cat /proc/meminfo
+
+echo -e "\n* Memory usage CPU vs GPU:"
+vcgencmd get_mem arm && vcgencmd get_mem gpu
+
+echo -e "\n* USB devices:"
+lsusb
+
+echo
